4ae24b0afeaaad873d61564417b69e486b68c7d6
saasbook <saasbook@saasbook.(none)>
2013-03-27 16:06:28 -0700

    v2.2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ActionMailer::Base
   def activation_email(user)
     @user = user
     #site_url="localhost:3000"
-    site_url="courseverse.com"
-    @url  = "http://#{site_url}/users/#{user.activation_token}/activate"
+    site_url="https://courseverse.herokuapp.com"
+    @url  = "#{site_url}/users/#{user.activation_token}/activate"
     mail(:to => user.email, :subject => "Welcome to CourseVerse")
   end
 end
